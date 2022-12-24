4e1ad57b12b0c168c600b597b694ace8fa86be39
lee <857992573@qq.com>
2022-12-24 20:22:09 +0800

    go 简易 blog

---
+++ logic/category.go
@@ -0,0 +1,19 @@
+package logic
+
+import (
+	"fmt"
+
+	"blog/dal/db"
+	"blog/model"
+)
+
+func GetAllCategoryList() (categoryList []*model.Category, err error) {
+	//1. 从数据库中，获取文章分类列表
+	categoryList, err = db.GetAllCategoryList()
+	if err != nil {
+		fmt.Printf("1 get article list failed, err:%v\n", err)
+		return
+	}
+
+	return
+}
--- main.go
+++ main.go
@@ -1,1 +1,50 @@
-package blog
+package main
+
+import (
+	//_ "net/http/pprof"
+	"github.com/DeanThompson/ginpprof"
+
+	"blog/controller"
+	"blog/dal/db"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+
+	dns := "root:123456@tcp(localhost:3306)/blog?parseTime=true"
+	err := db.Init(dns)
+	if err != nil {
+		panic(err)
+	}
+
+	ginpprof.Wrapper(router)
+	router.Static("/static/", "./static")
+	router.LoadHTMLGlob("views/*")
+
+	router.GET("/", controller.IndexHandle)
+	//发布文章页面
+	router.GET("/article/new/", controller.NewArticle)
+	//文章提交接口
+	router.POST("/article/submit/", controller.ArticleSubmit)
+	//文章详情页
+	router.GET("/article/detail/", controller.ArticleDetail)
+
+	//文件上传接口
+	router.POST("/upload/file/", controller.UploadFile)
+
+	//留言页面
+	router.GET("/leave/new/", controller.LeaveNew)
+	//关于我页面
+	router.GET("/about/me/", controller.AboutMe)
+
+	//文章评论相关
+	router.POST("/comment/submit/", controller.CommentSubmit)
+
+	//留言相关
+	router.POST("/leave/submit/", controller.LeaveSubmit)
+	//分类下面的文章列表
+	router.GET("/category/", controller.CategoryList)
+	router.Run(":8081")
+}
